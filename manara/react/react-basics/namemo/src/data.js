50836617a19a4bcea0f295b79e5629cb26ede650
mostafafinal <mostafafinal55@gmail.com>
2025-04-24 12:07:32 +0200

    init: Naruto Memory project initialization

---
+++ manara/react/react-basics/namemo/src/data.js
@@ -0,0 +1,12 @@
+export default [
+  { name: "Naruto Uzumaki", img: "./assets/characters/naruto.jpg" },
+  { name: "Sasuke Uchiha", img: "./assets/characters/sasuke.jpg" },
+  { name: "Kakashi Hatake", img: "./assets/characters/kakashi.jpg" },
+  { name: "Sakura Haruno", img: "./assets/characters/sakura.jpg" },
+  { name: "Hinata Hyuga", img: "./assets/characters/hinata.jpg" },
+  { name: "Ino Yamanaka", img: "./assets/characters/ino.jpg" },
+  { name: "Mikoto Uchiha", img: "./assets/characters/mikoto.jpg" },
+  { name: "Temari Rasa", img: "./assets/characters/temari.jpg" },
+  { name: "Tsunade Senju", img: "./assets/characters/tsunade.jpg" },
+  { name: "Madara Uchiha", img: "./assets/characters/madara.jpg" },
+];
