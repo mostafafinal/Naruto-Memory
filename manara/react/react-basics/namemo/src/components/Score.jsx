13289c337dd08390d88b7d108bd2978fdbd94658
mostafafinal <mostafafinal55@gmail.com>
2025-04-25 23:01:24 +0300

    feat: add score init component

---
+++ manara/react/react-basics/namemo/src/components/Score.jsx
@@ -0,0 +1,12 @@
+import "../styles/Score.css";
+
+const Score = () => {
+  return (
+    <div className="score">
+      <div className="score-player">Score: 0</div>
+      <div className="score-target">Target: 10</div>
+    </div>
+  );
+};
+
+export default Score;
