26a220406672d7963cf9d85cbae2f9c4a09586d3
mostafafinal <mostafafinal55@gmail.com>
2025-08-07 07:55:05 +0300

    fix: adjust shuffling issue

---
+++ src/lib/data.js
@@ -0,0 +1,14 @@
+import { v4 } from 'uuid';
+
+export default [
+  { id: v4(), name: 'Naruto Uzumaki', img: 'naruto.jpg' },
+  { id: v4(), name: 'Sasuke Uchiha', img: 'sasuke.jpg' },
+  { id: v4(), name: 'Kakashi Hatake', img: 'kakashi.jpg' },
+  { id: v4(), name: 'Sakura Haruno', img: 'sakura.jpg' },
+  { id: v4(), name: 'Hinata Hyuga', img: 'hinata.jpg' },
+  { id: v4(), name: 'Ino Yamanaka', img: 'ino.jpg' },
+  { id: v4(), name: 'Mikoto Uchiha', img: 'mikoto.jpg' },
+  { id: v4(), name: 'Temari Rasa', img: 'temari.jpg' },
+  { id: v4(), name: 'Tsunade Senju', img: 'tsunade.jpg' },
+  { id: v4(), name: 'Madara Uchiha', img: 'madara.jpg' },
+];
--- src/lib/utils/shuffler.js
+++ src/lib/utils/shuffler.js
@@ -1,11 +1,11 @@
-const shuffler = (arr) => {
+const shuffler = arr => {
   for (let i = arr.length - 1; i > 0; i--) {
     const randomNum = Math.floor(Math.random() * (i + 1));
 
     [arr[i], arr[randomNum]] = [arr[randomNum], arr[i]];
-
-    return arr;
   }
+
+  return arr;
 };
 
 export default shuffler;
